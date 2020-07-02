7e6b5e579dbf347d1e429a9509309a71cd9c6fd8
Richard Leek <richard@richardleek.com>
2020-07-02 12:06:59 -0400

    stuff

--- blocks.h
+++ blocks.h
@@ -3,15 +3,15 @@ static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
 	{"", "cat ~/.pacupdate | sed /📦0/d",					0,		9},
 	
-	{"🧠", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
+	{"", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
 
 	{"", "~/bin/statusbar/volume",						0,		10},
 
-	{"☀", "xbacklight | sed 's/\\..*//'",					0,		11},
+	{"", "xbacklight | sed 's/\\..*//'",					0,		11},
 	
-	{"", "~/bin/statusbar/battery",						5,		0},
+	{"", "~/bin/statusbar/battery",						5,		3},
 
-	{"🌡", "sensors | awk '/^temp1:/{print $2}'",				5,		0},
+	{"", "sensors | awk '/^temp1:/{print $2}'",				5,		0},
 
 	{"", "~/bin/statusbar/clock",						5,		0},
 };
